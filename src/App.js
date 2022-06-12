c931bb769e7020f1f3030b2525b9aab4a62eeb79
igorocky <123@gmail.com>
2022-06-12 16:01:34 +0200

    Add Cmp1.js and setup to serve prod build from localhost.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Cmp1 from "./Cmp1";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Cmp1/>
       </header>
     </div>
   );
---
+++ src/Cmp1.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Cmp1() {
+  return (
+      <button onClick={() => console.log('!@#!@#$%!#$%')}>Button</button>
+  );
+}
+
+export default Cmp1;
